e45a108f70f5721507aa776ad6d71078c9f4b474
Tim Vink <vinktim@gmail.com>
2020-06-04 21:03:58 +0200

    Hotfix for 13 (#19)
    * Update page.file source path with original source, fixes #12
    
    * Fix edgecase where monorepo does not copy any files to temp docs dir
    
    * Bump version and add unit tests for git related plugins
    
    * Fix git related unit tests
    
    * Fix keyerrors when docs contained folders
    
    * Fix unit test that was moved
    
    * bump version to 0.4.7
    
    * Python 3.5 compatibility when after using rglob

--- mkdocs_monorepo_plugin/merger.py
+++ mkdocs_monorepo_plugin/merger.py
@@ -17,8 +17,8 @@ from distutils.dir_util import copy_tree
 
 import logging
 import os
-from os import listdir
-from os.path import isfile, join
+from os.path import join
+from pathlib import Path
 
 from mkdocs.utils import warning_filter
 
@@ -60,11 +60,13 @@ class Merger:
 
             if os.path.exists(source_dir):
                 copy_tree(source_dir, dest_dir)
-                files = [f for f in listdir(source_dir) if isfile(join(source_dir, f))]
-                for f in files:
-                    src = join(source_dir, f)
-                    dest = join(dest_dir, f)
-                    self.files_source_dir[dest] = src
+                for file_abs_path in Path(source_dir).rglob('*.md'):
+                    file_abs_path = str(file_abs_path) # python 3.5 compatibility
+                    if os.path.isfile(file_abs_path):
+                        file_rel_path = os.path.relpath(file_abs_path, source_dir)
+                        dest = join(dest_dir, file_rel_path)
+                        self.files_source_dir[dest] = file_abs_path
+
             else:
                 log.critical(
                     "[mkdocs-monorepo] The {} path is not valid. ".format(source_dir) +
